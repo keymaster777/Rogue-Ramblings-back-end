534ac64b7059a621d32a149a45213f58d363d8c4
Riley Cleary <rileykeycleary@yahoo.com>
2021-02-15 06:54:19 -0700

    Setting up config for mongodb, adding more basic tests

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 const express = require('express')
+const bcrypt = require('bcrypt')
 const app = express()
+const mongoose = require('mongoose')
 
-const users = [
-  {
-    id: 1,
-    username: 'key777',
-    firstname: 'Riley',
-    lastname: 'Cleary',
-    passwordhash: 'adadsdf'
-  },
-  {
-    id: 2,
-    username: 'polio',
-    firstname: 'Niki',
-    lastname: 'Cleary',
-    passwordhash: 'aadsfsaf'
-  }
-]
+logger.info('connecting to', config.MONGODB_URI)
 
-app.get('/users', (request, response) => {
-  response.json(users)
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.get('/api/users/:id', (request, response) => {
+  const id = request.params.id
+  response.json(users[id])
 })
 
-app.get('/ping', (request, response) => {
+app.get('/api/ping', (request, response) => {
   response.json({ping: "pong"})
+})
+
+app.get('/api/hash/:password', async (request, response) => {
+  const saltRounds = 10
+  const passwordHashed = await bcrypt.hash(request.params.password, saltRounds)
+  response.send(passwordHashed)
 })
 
 module.exports = app
--- tests/users.test.js
+++ tests/users.test.js
@@ -1,13 +1,40 @@
-const { TestScheduler } = require('jest')
 const supertest = require('supertest')
 const app = require('../app')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 const api = supertest(app)
 
-test('api can be pinged', async () => {
-  const result = await api
-    .get('/ping')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-  
-    expect(result.body.ping).toContain('pong')
+describe('API can', () => {
+  const route = '/api'
+
+  test('api can be pinged', async () => {
+    const result = await api
+      .get(route+'/ping')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    
+      expect(result.body.ping).toContain('pong')
+  })
+
+  test('can hash a password', async () => {
+    const password = "sekret"
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const result = await api
+      .get(route+'/hash/'+password)
+      .expect(200)
+
+    const passwordsMatch = await bcrypt.compare(password, result.text) 
+    expect(passwordsMatch).toBe(true);
+  })
 })
+
+describe('User can', () => {
+  test('be retrieved from DB', async () => {
+    const result = await api
+      .get('/api/users/1')
+      .expect(200)
+  })
+})
+
